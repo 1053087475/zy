0d8057a7dc855916acaebbe239f428115916d102
余于鱼与渔 <50713838+1053087475@users.noreply.github.com>
2025-07-31 15:58:29 +0800

    Update checkin.js

--- checkin.js
+++ checkin.js
@@ -1,3 +1,10 @@
+'''
+const $ = new Env('IkUUU VPN流量统计')
+cron: 10 1 * * *
+创建 IKUUU_EMAIL 和 IKUUU_PWD 变量，多个邮箱或密码请换行。
+
+'''
+
 const axios = require('axios')
 const { getCookie, getTraffic, getEmailAndPwdList } = require('./utils')
 const notify = require('./sendNotify')
