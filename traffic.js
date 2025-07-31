470e0082ed1d7c4bde49d686229b6ec9f293a6ba
余于鱼与渔 <50713838+1053087475@users.noreply.github.com>
2025-07-31 16:46:07 +0800

    Update traffic.js

--- traffic.js
+++ traffic.js
@@ -1,9 +1,9 @@
-'''
-const $ = new Env('IkUUU VPN签到')
+/*
+new Env('IkUUU VPN流量统计')
 cron: 10 1 * * *
 创建 IKUUU_EMAIL 和 IKUUU_PWD 变量，多个邮箱或密码请换行。
 
-'''
+*/
 
 const { getCookie, getTraffic, getEmailAndPwdList } = require('./utils')
 const notify = require('./sendNotify');
