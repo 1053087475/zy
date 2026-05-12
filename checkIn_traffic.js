4e6652736680f50774e64890521a5828e43c0b9d
余于鱼与渔 <50713838+1053087475@users.noreply.github.com>
2026-05-12 21:47:35 +0800

    Add files via upload
    iKuuu VPN 签到、获取今日使用流量

--- checkIn_traffic.js
+++ checkIn_traffic.js
@@ -1,27 +1,12 @@
-/*
-new Env('IkUUU VPN流量统计')
-cron: 10 1 * * *
-创建 IKUUU_EMAIL 和 IKUUU_PWD 变量，多个邮箱或密码请换行。
-
-*/
-
-const { getCookie, getTraffic, getEmailAndPwdList } = require('./utils/utils')
-const notify = require('./utils/sendNotify');
+const { getTraffic, getCookieList } = require('./utils')
+const notify = require('./sendNotify');
 
 async function run() {
-  const [emailList, pwdList] = await getEmailAndPwdList()
+  const cookieList = await getCookieList()
   const messages = []
-  for (let i = 0; i < emailList.length; i++) {
-    const email = emailList[i]
-    const pwd = pwdList[i]
-    let msg = `邮箱：${emailList[i]}`
-    const cookie = await getCookie(email, pwd)
-    if (cookie.includes('登录失败')) {
-      msg += `\n${cookie}`
-      messages.push(msg)
-      continue
-    }
-    const arr = await getTraffic(cookie)
+  for (let i = 0; i < cookieList.length; i++) {
+    let msg = `账号 ${i + 1}`
+    const arr = await getTraffic(cookieList[i])
     msg += `\n${arr.join('\n')}`
     messages.push(msg)
   }
